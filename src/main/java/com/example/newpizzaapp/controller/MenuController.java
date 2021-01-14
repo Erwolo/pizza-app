e9b7ba180f706e959182e0dab8bd136ad1899c58
Erwol <konkelerwin@gmail.com>
2021-01-14 21:12:55 +0100

    Menu lepiej skalowalne

--- src/main/java/com/example/newpizzaapp/controller/MenuController.java
+++ src/main/java/com/example/newpizzaapp/controller/MenuController.java
@@ -1,26 +1,30 @@
 package com.example.newpizzaapp.controller;
 
 import com.example.newpizzaapp.model.Food;
+import com.example.newpizzaapp.services.FoodCategoryService;
 import com.example.newpizzaapp.services.FoodService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 
 @Controller
 public class MenuController {
 
     private final FoodService foodService;
+    private final FoodCategoryService foodCategoryService;
     private final OrderController orderController;
 
-    public MenuController(FoodService foodService, OrderController orderController) {
+    public MenuController(FoodService foodService, OrderController orderController, FoodCategoryService foodCategoryService) {
         this.foodService = foodService;
         this.orderController = orderController;
+        this.foodCategoryService = foodCategoryService;
     }
 
     @GetMapping("/menu")
     public String displayMenu(Model model) {
+        model.addAttribute("foodService", foodService);
+        model.addAttribute("allCategories", foodCategoryService.getAllCategories());
         model.addAttribute("pizzaList", foodService.getAllFromCategory("Pizza"));
         model.addAttribute("courseList", foodService.getAllFromCategory("Danie Glowne"));
         model.addAttribute("snackList", foodService.getAllFromCategory("Przekaski"));
